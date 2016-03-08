c251faa2d406185db75459e15a37f5178493176c
Bradley <BAJakubowski@gmail.com>
2016-03-08 12:35:03 -0600

    First upload, Dice Class added

---
+++ src/diceGame/Dice.java
@@ -0,0 +1,38 @@
+package diceGame;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Dice{
+	
+
+	public int DFour()
+	{
+		int dFour = ThreadLocalRandom.current().nextInt(1,5);
+		return dFour;
+	}
+	public int DSix()
+	{
+		int dSix = ThreadLocalRandom.current().nextInt(1,7);
+		return dSix;
+	}
+	public int DEight()
+	{
+		int dEight = ThreadLocalRandom.current().nextInt(1,9);
+		return dEight;
+	}
+	public int DTen()
+	{
+		int dTen = ThreadLocalRandom.current().nextInt(1,11);
+		return dTen;
+	}
+	public int DTwelve()
+	{
+		int dTwelve = ThreadLocalRandom.current().nextInt(1,13);
+		return dTwelve;
+	}
+	public int DTwenty()
+	{
+		int dTwenty = ThreadLocalRandom.current().nextInt(1,21);
+		return dTwenty;
+	}
+}
