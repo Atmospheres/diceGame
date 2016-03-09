416336a947c61a5ddbc81fc4a73246c0068a6268
Bradley <BAJakubowski@gmail.com>
2016-03-09 09:34:52 -0600

    SetPlayerElement function

--- src/diceGame/Player.java
+++ src/diceGame/Player.java
@@ -39,4 +39,34 @@ public class Player
 		mana += manaIncrease;
 		System.out.println("After 5 rolls of the four-sided die, your total mana increase is " + manaIncrease + " mana.");
 	}
+	public String SetPlayerElement()
+	{
+			int roll = dice.DSix();
+			switch(roll)
+			{
+				case 1:
+					currentElement = "Fire";
+					break;
+				case 2:
+					currentElement = "Ice";
+					break;
+				case 3:
+					currentElement = "Wind";
+					break;
+				case 4:
+					currentElement = "Earth";
+					break;
+				case 5:
+					currentElement = "Lightning";
+					break;
+				case 6:
+					currentElement = "Water";
+					break;
+				default:
+					currentElement = "Fire"
+					break;
+			
+			}
+			return currentElement;
+	}
 }		
